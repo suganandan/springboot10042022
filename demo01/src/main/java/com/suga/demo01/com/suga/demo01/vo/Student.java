a5297eb6e29936537b520e3522e80f6952e30ffd
suganandan <suganandan@hotmail.com>
2022-04-10 18:44:32 +0530

    my second commit

--- demo01/src/main/java/com/suga/demo01/com/suga/demo01/vo/Student.java
+++ demo01/src/main/java/com/suga/demo01/com/suga/demo01/vo/Student.java
@@ -1,5 +1,13 @@
 package com.suga.demo01.com.suga.demo01.vo;
 
 public class Student {
+private String userName;
 
+public String getUserName() {
+	return userName;
+}
+
+public void setUserName(String userName) {
+	this.userName = userName;
+}
 }
